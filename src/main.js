515b2023fbba380865ff85494e859072d5e9b7e9
Kondratenko Maks <exollerex@gmail.com>
2025-08-18 23:23:16 +0700

    update

--- src/main.js
+++ src/main.js
@@ -4,31 +4,31 @@ import './styles/styles.scss';
 import App from './App.vue';
 import router from './router';
 import { persistPlugin } from '@/core/plugins/persistPlugin';
-import Toast from 'vue-toastification';
+// import Toast from 'vue-toastification';
 
-// import sprite from '@/assets/sprites/sprite-093e2613.svg?raw';
-// document.body.insertAdjacentHTML(
-//   'beforeend',
-//   sprite.replace('<svg', '<svg style="display:none"')
-// );
+import sprite from '@/assets/sprites/sprite-093e2613.svg?raw';
+document.body.insertAdjacentHTML(
+  'beforeend',
+  sprite.replace('<svg', '<svg style="display:none"')
+);
 
 createApp(App)
   .use(createPinia().use(persistPlugin))
   .use(router)
-  .use(Toast, {
-    position: 'top-right',
-    timeout: 4_000,
-    closeOnClick: true,
-    pauseOnFocusLoss: false,
-    pauseOnHover: false,
-    draggable: false,
-    draggablePercent: 0,
-    showCloseButtonOnHover: false,
-    hideProgressBar: false,
-    closeButton: false,
-    icon: false,
-    rtl: false,
-    maxToasts: 5,
-    newestOnTop: true,
-  })
+  // .use(Toast, {
+  //   position: 'top-right',
+  //   timeout: 4_000,
+  //   closeOnClick: true,
+  //   pauseOnFocusLoss: false,
+  //   pauseOnHover: false,
+  //   draggable: false,
+  //   draggablePercent: 0,
+  //   showCloseButtonOnHover: false,
+  //   hideProgressBar: false,
+  //   closeButton: false,
+  //   icon: false,
+  //   rtl: false,
+  //   maxToasts: 5,
+  //   newestOnTop: true,
+  // })
   .mount('#app');
