a9db9b89f7a764640b551bc1dce74f10a375c411
Kondratenko Maks <exollerex@gmail.com>
2025-08-19 00:51:45 +0700

    createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import DashboardView from '@/views/DashboardView.vue';
 
 const routes = [
@@ -40,7 +40,7 @@ const routes = [
 
 const router = createRouter({
   // history: createWebHistory(),
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to) {
     if (to.hash) {
