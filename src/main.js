ff1428c33a873cf40baedfd78781ccfc48768ca9
Kondratenko Maks <exollerex@gmail.com>
2025-08-23 18:02:23 +0700

    update configs

--- src/main.js
+++ src/main.js
@@ -6,11 +6,11 @@ import router from './router';
 import { persistPlugin } from '@/core/plugins/persistPlugin';
 import Toast from 'vue-toastification';
 
-// import sprite from '@/assets/sprites/sprite-093e2613.svg?raw';
-// document.body.insertAdjacentHTML(
-//   'beforeend',
-//   sprite.replace('<svg', '<svg style="display:none"')
-// );
+import sprite from '@/assets/sprites/sprite-fd8b7d9a.svg?raw';
+document.body.insertAdjacentHTML(
+  'beforeend',
+  sprite.replace('<svg', '<svg style="display:none"')
+);
 
 createApp(App)
   .use(createPinia().use(persistPlugin))
