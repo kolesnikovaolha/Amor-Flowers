812b0253084ee815db8b37a7ee263887348a163a
Kolesnikova Olha <kolesnikovaolha8@gmail.com>
2025-08-24 10:04:34 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/Amor-Flowers/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/Amor-Flowers/' : '/',
+  publicPath: './',
   outputDir: 'docs',
   css: {
     loaderOptions: {
